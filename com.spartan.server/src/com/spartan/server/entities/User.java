5cf7b28d80df2d073968ce307673f096d0abb826
hellspawn14 <hellspawn@MacBook-Pro-de-Andres.local>
2014-10-14 00:20:58 -0500

    Implementacion del servidor

--- com.spartan.server/src/com/spartan/server/entities/User.java
+++ com.spartan.server/src/com/spartan/server/entities/User.java
@@ -7,28 +7,99 @@ package com.spartan.server.entities;
 public class User 
 {
 	//------------------------------------------------------------------
-	//Constantes
-	//------------------------------------------------------------------
-
-	//------------------------------------------------------------------
 	//Atributos
 	//------------------------------------------------------------------
 	
+	/**
+	 * Identificador del usuario en base de datos 
+	 */
+	private String userId;
+	
+	/**
+	 * Nombre de usuario
+	 */
 	private String userName;
 	
-	
-	
-	
+	/**
+	 * Perfil del usuario
+	 */
+	private UserProfile profile;
+
 	//------------------------------------------------------------------
 	//Construtores
 	//------------------------------------------------------------------
 
-	//------------------------------------------------------------------
-	//Metodos
-	//------------------------------------------------------------------
-
+	/**
+	 * Crea un nuevo usuario en el sistema
+	 * @param userId - Es el identificador del usuario
+	 * @param userName - Es el nombre del usuario
+	 * @param profile - Es el perfil 
+	 */
+	public User(String userId, String userName, UserProfile profile) 
+	{
+		this.userId = userId;
+		this.userName = userName;
+		this.profile = profile;
+	}
+	
 	//------------------------------------------------------------------
 	//Getters & Setters
 	//------------------------------------------------------------------
+	
+
+	/**
+	 * @return the userId
+	 */
+	public String getUserId() {
+		return userId;
+	}
+
+
+
+	/**
+	 * @param userId the userId to set
+	 */
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+
+
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+
+
+
+	/**
+	 * @param userName the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+
+
+	/**
+	 * @return the profile
+	 */
+	public UserProfile getProfile() {
+		return profile;
+	}
+
+
+
+	/**
+	 * @param profile the profile to set
+	 */
+	public void setProfile(UserProfile profile) {
+		this.profile = profile;
+	}
+
+	
+
 
 }
---
+++ com.spartan.server/src/com/spartan/server/entities/UserProfile.java
@@ -0,0 +1,327 @@
+package com.spartan.server.entities;
+
+/**
+ * Representa el perfil de un usuario 
+ * @author hellspawn
+ */
+public class UserProfile 
+{
+	//------------------------------------------------------------------
+	//Atributos
+	//------------------------------------------------------------------
+
+	/**
+	 * Nombre real del usuario
+	 */
+	private String name;
+	
+	/**
+	 * Apellido real del usuario 
+	 */
+	private String lastName;
+	
+	/**
+	 * Numero telefonico del usuario (preferiblemente celular)
+	 */
+	private String phoneNumber;
+	
+	/**
+	 * Puntaje del usuario 
+	 */
+	private double score;
+	
+	/**
+	 * Numero de juegos de soccer
+	 */
+	private int nSoccer;
+	
+	/**
+	 * Numero de juegos de basketball 
+	 */
+	private int nBasketball;
+	
+	/**
+	 * Numero de juegos de voleyball
+	 */
+	private int nVoleyball;
+	
+	/**
+	 * Numero de juegos de tennis
+	 */
+	private int nTennis;
+	
+	/**
+	 * Numero de juegos de paintball
+	 */
+	private int nPaintball;
+	
+	/**
+	 * Numero de juegos de videojuegos
+	 */
+	private int nVideogames;
+	
+	/**
+	 * Numero de juegos de karts 
+	 */
+	private int nKarts;
+	
+	/**
+	 * Numero de victorias
+	 */
+	private int nVictories;
+	
+	/**
+	 * Numero de derrotas
+	 */
+	private int nLoses;
+
+	
+	//------------------------------------------------------------------
+	//Construtores
+	//------------------------------------------------------------------
+
+	/**
+	 * Crea un nuevo perfil de usuario en el sistema
+	 * @param name - Es el nombre real de usuario 
+	 * @param lastName - Es el apellido real del usuario 
+	 * @param phoneNumber - Es el numero de telefono de usuario 
+	 * @param score - Es el puntaje del usuario 
+	 * @param nSoccer - Es el numero de encuentros de partidos de soccer
+	 * @param nBasketball - Es el numero de encuentros de basketball
+	 * @param nVoleyball - Es el numero de encuentros de Voleyball 
+	 * @param nTennis - Es el numero de encuentros de tennis
+	 * @param nPaintball - Es el numero de encuentros de paintball
+	 * @param nVideogames - Es el numero de encuentros de videojuegos
+	 * @param nKarts - Es el numero de encuentros de karts 
+	 * @param nVictories - Es el numero de victorias obtenidas
+	 * @param nLoses - Es el numero de derrotas
+	 */
+	public UserProfile(String name, String lastName, String phoneNumber,double score, int nSoccer, int nBasketball, int nVoleyball,int nTennis, int nPaintball, int nVideogames, int nKarts,int nVictories, int nLoses)
+	{
+		this.name = name;
+		this.lastName = lastName;
+		this.phoneNumber = phoneNumber;
+		this.score = score;
+		this.nSoccer = nSoccer;
+		this.nBasketball = nBasketball;
+		this.nVoleyball = nVoleyball;
+		this.nTennis = nTennis;
+		this.nPaintball = nPaintball;
+		this.nVideogames = nVideogames;
+		this.nKarts = nKarts;
+		this.nVictories = nVictories;
+		this.nLoses = nLoses;
+	}
+
+	//------------------------------------------------------------------
+	//Getters & Setters
+	//------------------------------------------------------------------
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	/**
+	 * @return the phoneNumber
+	 */
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+
+	/**
+	 * @param phoneNumber the phoneNumber to set
+	 */
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+
+	/**
+	 * @return the score
+	 */
+	public double getScore() {
+		return score;
+	}
+
+
+	/**
+	 * @param score the score to set
+	 */
+	public void setScore(double score) {
+		this.score = score;
+	}
+
+
+	/**
+	 * @return the nSoccer
+	 */
+	public int getnSoccer() {
+		return nSoccer;
+	}
+
+
+	/**
+	 * @param nSoccer the nSoccer to set
+	 */
+	public void setnSoccer(int nSoccer) {
+		this.nSoccer = nSoccer;
+	}
+
+
+	/**
+	 * @return the nBasketball
+	 */
+	public int getnBasketball() {
+		return nBasketball;
+	}
+
+
+	/**
+	 * @param nBasketball the nBasketball to set
+	 */
+	public void setnBasketball(int nBasketball) {
+		this.nBasketball = nBasketball;
+	}
+
+
+	/**
+	 * @return the nVoleyball
+	 */
+	public int getnVoleyball() {
+		return nVoleyball;
+	}
+
+
+	/**
+	 * @param nVoleyball the nVoleyball to set
+	 */
+	public void setnVoleyball(int nVoleyball) {
+		this.nVoleyball = nVoleyball;
+	}
+
+
+	/**
+	 * @return the nTennis
+	 */
+	public int getnTennis() {
+		return nTennis;
+	}
+
+
+	/**
+	 * @param nTennis the nTennis to set
+	 */
+	public void setnTennis(int nTennis) {
+		this.nTennis = nTennis;
+	}
+
+
+	/**
+	 * @return the nPaintball
+	 */
+	public int getnPaintball() {
+		return nPaintball;
+	}
+
+
+	/**
+	 * @param nPaintball the nPaintball to set
+	 */
+	public void setnPaintball(int nPaintball) {
+		this.nPaintball = nPaintball;
+	}
+
+
+	/**
+	 * @return the nVideogames
+	 */
+	public int getnVideogames() {
+		return nVideogames;
+	}
+
+
+	/**
+	 * @param nVideogames the nVideogames to set
+	 */
+	public void setnVideogames(int nVideogames) {
+		this.nVideogames = nVideogames;
+	}
+
+
+	/**
+	 * @return the nKarts
+	 */
+	public int getnKarts() {
+		return nKarts;
+	}
+
+
+	/**
+	 * @param nKarts the nKarts to set
+	 */
+	public void setnKarts(int nKarts) {
+		this.nKarts = nKarts;
+	}
+
+
+	/**
+	 * @return the nVictories
+	 */
+	public int getnVictories() {
+		return nVictories;
+	}
+
+
+	/**
+	 * @param nVictories the nVictories to set
+	 */
+	public void setnVictories(int nVictories) {
+		this.nVictories = nVictories;
+	}
+
+
+	/**
+	 * @return the nLoses
+	 */
+	public int getnLoses() {
+		return nLoses;
+	}
+
+
+	/**
+	 * @param nLoses the nLoses to set
+	 */
+	public void setnLoses(int nLoses) {
+		this.nLoses = nLoses;
+	}
+
+}

0ca8173e687c77caf4235c2f046b37f21c9f4b04
whoGirl <dm.martinez831@uniandes.edu.co>
2014-10-18 14:51:33 -0500

    basic event operations...

--- com.spartan.server/src/com/spartan/server/operations/EventOperations.java
+++ com.spartan.server/src/com/spartan/server/operations/EventOperations.java
@@ -1,134 +1,156 @@
 package com.spartan.server.operations;
 
+import java.sql.SQLException;
 import java.util.Date;
 
 import com.spartan.server.core.DateParse;
 import com.spartan.server.db.DBService;
 import com.spartan.server.db.ScriptGenerator;
 import com.spartan.server.entities.Event;
+import com.spartan.server.enums.State;
 
 /**
- * Conjunto de operaciones soportadas por un evento 
+ * Conjunto de operaciones soportadas por un evento
+ * 
  * @author hellspawn
  */
-public class EventOperations 
-{
-	//------------------------------------------------------------------
-	//Atributos 
-	//------------------------------------------------------------------
+public class EventOperations {
+	// ------------------------------------------------------------------
+	// Atributos
+	// ------------------------------------------------------------------
 
 	/**
 	 * Relacion con el parser de fecha
 	 */
 	private DateParse dateParse;
-	
+
 	/**
 	 * Relacon con la conexion con la db
 	 */
 	private DBService dbService;
-	
+
 	/**
 	 * Relacion con el generador de scripts
 	 */
 	private ScriptGenerator scriptGen;
-	
-	//------------------------------------------------------------------
-	//Constructores 
-	//------------------------------------------------------------------
+
+	// ------------------------------------------------------------------
+	// Constructores
+	// ------------------------------------------------------------------
 
 	/**
 	 * Crea un nuevo operador de eventos
-	 * @param dateParse - Servicio de transformacion de fechas
-	 * @param dbService - Servicio de base de datos 
-	 * @param scriptGen - Servicio de generacion de scripts SQL
+	 * 
+	 * @param dateParse
+	 *            - Servicio de transformacion de fechas
+	 * @param dbService
+	 *            - Servicio de base de datos
+	 * @param scriptGen
+	 *            - Servicio de generacion de scripts SQL
 	 */
-	public EventOperations(DateParse dateParse, DBService dbService, ScriptGenerator scriptGen)
-	{
+	public EventOperations(DateParse dateParse, DBService dbService,
+			ScriptGenerator scriptGen) {
 		this.dateParse = dateParse;
 		this.dbService = dbService;
 		this.scriptGen = scriptGen;
 	}
-	
-	
-	//------------------------------------------------------------------
-	//Operaciones generales 
-	//------------------------------------------------------------------
-	
-	
-	
+
+	// ------------------------------------------------------------------
+	// Operaciones generales
+	// ------------------------------------------------------------------
+
 	/**
-	 * Cambia la fecha de un evento 
+	 * Cambia la fecha de un evento
 	 */
-	public synchronized void changeDate(Event E, String nDate) throws Exception 
-	{
+	public synchronized void changeDate(Event E, String nDate) throws Exception {
 		Date today = new Date();
 		Date toChange = dateParse.StringToDate(nDate);
-		
-		
-		
-		//Compara las dos fechas
-		
+		if (toChange.after(toChange)) {
+			E.setDate(nDate);
+			// String q =scriptGen.updateEvent(asda);
+			// dbService.sendQueryToDB(q);
+		}
+
+		// Compara las dos fechas
+
 	}
-	
-	public static void main (String args[])
-	{
+
+	public static void main(String args[]) {
 	}
 
 	/**
-	 * Cambia los detalles generales de un evento 
+	 * Cambia los detalles generales de un evento
 	 */
-	public void changeDetails(Event E, String details)
-	{
-		
+	public void changeDetails(Event E, String details) {
+		E.setDetails(details);
 	}
-	
+
 	/**
 	 * Cambia el estado de un evento (ver com.spartan.server.enums)
 	 */
-	public void changeState(Event E, String state)
-	{
-		
+	public void changeState(Event E, String state) {
+
+		// flujo normal de eventos
+		if ((E.getState().equals(State.OPEN) && state.equals(State.TO_START))
+				|| (E.getState().equals(State.TO_START) && state
+						.equals(State.STARTED))
+				|| (E.getState().equals(State.STARTED) && state
+						.equals(State.ENDED))) {
+			E.setState(state);
+			// String q =scriptGen.updateEvent(E);
+			// dbService.sendQueryToDB(q);
+		}
+
 	}
-	
+
 	/**
-	 * Cancela un evento 
+	 * Cancela un evento
 	 */
-	public void cancelEvent(Event E)
-	{
-		
+	public void cancelEvent(Event E) {
+		String q = scriptGen.cancelEvent(E.getIdEvent());
+		try {
+			dbService.sendQueryToDB(q);
+		} catch (SQLException e) {
+			// nothing
+		}
 	}
-	
+
 	/**
-	 * Cambia la localizacion de un evento 
+	 * Cambia la localizacion de un evento
 	 */
-	public void changeLocation(Event E, String location)
-	{
-		
+	public void changeLocation(Event E, String location) {
+		E.setPlace(location);
+//		String q = scriptGen.updateEvent(E);
+//		dbService.sendQueryToDB(q);
 	}
-	
+		
 	/**
 	 * Actualiza el numero de lugares disponibles
 	 */
-	public void upgradeSpots(Event E, int spots)  
-	{
-		
+	public void upgradeSpots(Event E, int spots) {
+		E.setSpots(spots);
+//		String q = scriptGen.updateEvent(E);
+//		dbService.sendQueryToDB(q);
 	}
-	
+
 	/**
 	 * Reduce el numero de lugares disponibles
 	 */
-	public void downgradeSpots(Event E)
-	{
-		
+	public void downgradeSpots(Event E) {
+		int spots = E.getSpots();
+		if(spots>0)
+		{
+			E.setSpots(spots-1);
+//			String q = scriptGen.updateEvent(E);
+//			dbService.sendQueryToDB(q);
+		}
+
 	}
-		
-	//------------------------------------------------------------------
-	//Regular event
-	//------------------------------------------------------------------
+
+	// ------------------------------------------------------------------
+	// Regular event
+	// ------------------------------------------------------------------
+	
 	
 
-	
-	
-	
-	
 }

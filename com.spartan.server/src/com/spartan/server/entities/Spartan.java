4c0d6232be9a39c4d2defea87265a9d1dfc160f3
hellspawn14 <hellspawn@192.168.0.26>
2014-10-18 18:54:00 -0500

    Cambios menores

--- com.spartan.server/src/com/spartan/server/entities/Spartan.java
+++ com.spartan.server/src/com/spartan/server/entities/Spartan.java
@@ -2,37 +2,49 @@ package com.spartan.server.entities;
 
 import java.util.Hashtable;
 
+/**
+ * Representa la clase principal de las entidades 
+ * @author hellspawn
+ */
 public class Spartan 
 {
-	
 	//------------------------------------------------------------------
 	//Atributos
 	//------------------------------------------------------------------
 
 	/**
 	 * Lista de usuarios activos
-	 * Key -> Id
+	 * Key -> Identificador usuario 
 	 */
 	private Hashtable <Integer, User> activeUsers;
 	
 	/**
 	 * Lista de equipos registrados
-	 * Key  -> Team name
+	 * Key  -> Identificador del equipo 
 	 */
 	private Hashtable <Integer, Team> registeredTeams;
 	
 	/**
 	 * Lista de eventos actuales
+	 * Key -> Identificador del evento 
 	 */
-	private Hashtable <Integer, Event> currentEvents;
+	private Hashtable <Integer, RegularEvent> currentEvents;
 	
 	/**
-	 * Lista de torneos actuales
+	 * Lista de torneos grupales actuales
+	 * Key -> Identificador del torneo grupal 
 	 */
-	private Hashtable <Integer, Tournament> currentTournaments;
+	private Hashtable <Integer, GroupTournament> groupTournaments;
+	
+	/**
+	 * Lista de torneos individuales actuales 
+	 * Key -> Identificador del torneo individual
+	 */
+	private Hashtable <Integer, SoloTournament> soloTournaments;
 	
 	/**
 	 * Lista de eventos publicos 
+	 * Key -> Identificador del evento publico
 	 */
 	private Hashtable <Integer, PublicEvent> publicCatalog;
 
@@ -41,8 +53,19 @@ public class Spartan
 	//Construtores
 	//------------------------------------------------------------------
 	
-	
-	
+	/**
+	 * Crea un nuevo Spartan 
+	 * Inicializa las estructuras de datos 
+	 */
+	public Spartan()
+	{
+		activeUsers = new Hashtable <Integer, User>();
+		registeredTeams = new Hashtable <Integer, Team>();
+		currentEvents = new Hashtable <Integer, RegularEvent>();
+		groupTournaments = new Hashtable <Integer, GroupTournament>();
+		soloTournaments = new Hashtable <Integer, SoloTournament>();
+		publicCatalog = new Hashtable <Integer, PublicEvent>();
+	}
 	
 	//------------------------------------------------------------------
 	//Getters & Setters
@@ -55,12 +78,14 @@ public class Spartan
 		return activeUsers;
 	}
 
+
 	/**
 	 * @param activeUsers the activeUsers to set
 	 */
 	public void setActiveUsers(Hashtable<Integer, User> activeUsers) {
 		this.activeUsers = activeUsers;
 	}
+
 
 	/**
 	 * @return the registeredTeams
@@ -69,6 +94,7 @@ public class Spartan
 		return registeredTeams;
 	}
 
+
 	/**
 	 * @param registeredTeams the registeredTeams to set
 	 */
@@ -76,49 +102,69 @@ public class Spartan
 		this.registeredTeams = registeredTeams;
 	}
 
+
 	/**
 	 * @return the currentEvents
 	 */
-	public Hashtable<Integer, Event> getCurrentEvents() {
+	public Hashtable<Integer, RegularEvent> getCurrentEvents() {
 		return currentEvents;
 	}
+
 
 	/**
 	 * @param currentEvents the currentEvents to set
 	 */
-	public void setCurrentEvents(Hashtable<Integer, Event> currentEvents) {
+	public void setCurrentEvents(Hashtable<Integer, RegularEvent> currentEvents) {
 		this.currentEvents = currentEvents;
 	}
 
-	/**
-	 * @return the currentTournaments
-	 */
-	public Hashtable<Integer, Tournament> getCurrentTournaments() {
-		return currentTournaments;
-	}
 
 	/**
-	 * @param currentTournaments the currentTournaments to set
+	 * @return the groupTournaments
 	 */
-	public void setCurrentTournaments(
-			Hashtable<Integer, Tournament> currentTournaments) {
-		this.currentTournaments = currentTournaments;
+	public Hashtable<Integer, GroupTournament> getGroupTournaments() {
+		return groupTournaments;
 	}
+
+
+	/**
+	 * @param groupTournaments the groupTournaments to set
+	 */
+	public void setGroupTournaments(
+			Hashtable<Integer, GroupTournament> groupTournaments) {
+		this.groupTournaments = groupTournaments;
+	}
+
+
+	/**
+	 * @return the soloTournaments
+	 */
+	public Hashtable<Integer, SoloTournament> getSoloTournaments() {
+		return soloTournaments;
+	}
+
+
+	/**
+	 * @param soloTournaments the soloTournaments to set
+	 */
+	public void setSoloTournaments(
+			Hashtable<Integer, SoloTournament> soloTournaments) {
+		this.soloTournaments = soloTournaments;
+	}
+
 
 	/**
 	 * @return the publicCatalog
 	 */
-	public Hashtable <Integer, PublicEvent> getPublicCatalog() {
+	public Hashtable<Integer, PublicEvent> getPublicCatalog() {
 		return publicCatalog;
 	}
 
 	/**
 	 * @param publicCatalog the publicCatalog to set
 	 */
-	public void setPublicCatalog(Hashtable <Integer, PublicEvent> publicCatalog) {
+	public void setPublicCatalog(Hashtable<Integer, PublicEvent> publicCatalog) {
 		this.publicCatalog = publicCatalog;
 	}
 	
-	
-
 }
